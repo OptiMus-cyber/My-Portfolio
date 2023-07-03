ee01e5f3752d3485719207038964e7c59343ddd8
Prakash Singh Rautela <56201964+OptiMus-cyber@users.noreply.github.com>
2023-07-03 22:54:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right' });
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text', {
-     strings: ['Software Developer','Web Developer'],
+     strings: ['Software Developer','Web Developer','Freelancer'],
      typeSpeed: 100,
      backSpeed: 100,
      backDelay: 1000,
